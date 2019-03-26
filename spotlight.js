28caff0752225105dc0e9826662a9baee09486a0
Oliver Göldi <olivergoldi@me.com>
2019-03-26 14:28:35 +0100

    add color support

--- spotlight.js
+++ spotlight.js
@@ -9,15 +9,15 @@ function Spotlight (config) {
         
         var style = document.createElement('style');
         style.innerHTML =
-        	'#spotlight {' +
-                'position:fixed;' +
-                'z-index: 100;' +
+        	`#spotlight {` +
+                `position:fixed;` +
+                `z-index: 100;` +
         		`height: ${config.height}px;` +
                 `width: ${config.width}px;` +
-                'outline: 200vh solid black;' +
-                `background: radial-gradient(ellipse at center, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 40%,rgba(0,0,0,1) 60%,rgba(0,0,0,1) 100%);` +
-                'cursor: crosshair;' +
-                'pointer-events: none;' +
+                `outline: 200vh solid ${this.convertToRGBA(config.color)};` +
+                `background: radial-gradient(ellipse at center, ${this.convertToRGBA(config.color, 0)} 0%,rgba(0,0,0,0) 40%,${this.convertToRGBA(config.color, 1)} 60%,${this.convertToRGBA(config.color, 1)} 100%);` +
+                `cursor: crosshair;` +
+                `pointer-events: none;` +
         	'}';
         document.getElementsByTagName('head')[0].appendChild(style, ref);
     }
@@ -39,12 +39,25 @@ Spotlight.prototype.isEnabled = function (event) {
         .split("&")
         .forEach(function (item) {
             tmp = item.split('=');
-            if (tmp[0] === 'cool' && tmp[1] === 'yo') result = decodeURIComponent(tmp[1]);
+            if (tmp[0] === 'cool') result = decodeURIComponent(tmp[1]);
         });
     return (result === 'yo');
+}
+
+Spotlight.prototype.convertToRGBA = function(hexValue, alpha) {
+    let r = parseInt(hexValue.slice(1,3), 16);
+    let g = parseInt(hexValue.slice(3,5), 16);
+    let b = parseInt(hexValue.slice(5,7), 16);
+    
+    if(alpha === undefined) {
+        alpha = 1;
+    }
+    
+    return `rgba(${r},${g},${b},${alpha})`
 }
 
 window.spotlight = new Spotlight({
     height: 400,
     width: 400
+    color: "#f1f442"
 });
